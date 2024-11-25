297f2c5724e38e8f5982936d7b5adcb10750eaf4
Waleed-AlSomali <waleedy93@gmail.com>
2024-11-25 16:37:51 +0300

    Last image optimization

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -10,27 +10,36 @@ const Product = ({ product }) => {
     const { addToCart } = useCart();
 
     return (
-
-        <Grid item xs={12} sm={6} md={4} >
-            <Card sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-                <CardMedia component="img"
+        <Grid item xs={12} sm={6} md={4}>
+            <Card
+                sx={{
+                    display: 'flex',
+                    flexDirection: 'column',
+                    height: '100%',
+                    boxShadow: 3, 
+                    borderRadius: 2, 
+                    transition: 'box-shadow 0.3s ease-in-out', 
+                    '&:hover': {
+                        boxShadow: 6,
+                    },
+                }}
+            >
+                <CardMedia
+                    component="img"
                     height="200"
                     src={product.imageUrl}
                     alt={product.title}
-                    sx={{ objectFit: 'contain' }}>
-                </CardMedia>
-                <CardContent >
-                    <Typography variant='h5'> {product.title}</Typography>
+                    sx={{ objectFit: 'contain' }}
+                />
+                <CardContent>
+                    <Typography variant="h5">{product.title}</Typography>
                     <Typography>Quantity = {product.quantity}</Typography>
                     <Typography>Price = {product.price}$</Typography>
                     <Typography>{product.description}</Typography>
-                    <Button
-                        fullWidth
-                        sx={{ marginTop: '16px' }}>
-                        {<Link to={`products/${product.productID}`}>Show details</Link>}
+                    <Button fullWidth sx={{ marginTop: '16px' }}>
+                        <Link to={`products/${product.productID}`}>Show details</Link>
                     </Button>
                     <Button
-
                         variant="contained"
                         color="secondary"
                         fullWidth
