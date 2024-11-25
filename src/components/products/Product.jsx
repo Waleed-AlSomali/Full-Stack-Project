8a6335208400ecd174bcc5d2de28238487ff9446
Waleed-AlSomali <waleedy93@gmail.com>
2024-11-25 16:19:12 +0300

    another image variation v3

--- src/components/products/Product.jsx
+++ src/components/products/Product.jsx
@@ -11,40 +11,37 @@ const Product = ({ product }) => {
 
     return (
 
-        <Grid container spacing={3}>
-    <Grid item xs={12} sm={6} md={4}>
-        <Card sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
-            <CardMedia
-                component="img"
-                height="180"
-                src={product.imageUrl}
-                alt={product.title}
-                sx={{ objectFit: 'contain', marginBottom: 2 }}
-            />
-            <CardContent sx={{ display: 'flex', flexDirection: 'column', flexGrow: 1 }}>
-                <Typography variant="h5">{product.title}</Typography>
-                <Typography>Quantity = {product.quantity}</Typography>
-                <Typography>Price = {product.price}$</Typography>
-                <Typography>{product.description}</Typography>
-                <Button
-                    fullWidth
-                    sx={{ marginTop: 2 }}
-                >
-                    <Link to={`products/${product.productID}`}>Show details</Link>
-                </Button>
-                <Button
-                    variant="contained"
-                    color="secondary"
-                    fullWidth
-                    sx={{ marginTop: 2 }} 
-                    onClick={() => addToCart(product)}
-                >
-                    Add To Cart
-                </Button>
-            </CardContent>
-        </Card>
-    </Grid>
-</Grid>
+        <Grid item xs={12} sm={6} md={4} >
+            <Card sx={{ display: 'flex', flexDirection: 'column', height: '100%' }}>
+                <CardMedia component="img"
+                    height="180"
+                    src={product.imageUrl}
+                    alt={product.title}
+                    sx={{ objectFit: 'contain' }}>
+                </CardMedia>
+                <CardContent >
+                    <Typography variant='h5'> {product.title}</Typography>
+                    <Typography>Quantity = {product.quantity}</Typography>
+                    <Typography>Price = {product.price}$</Typography>
+                    <Typography>{product.description}</Typography>
+                    <Button
+                        fullWidth
+                        sx={{ marginTop: '16px' }}>
+                        {<Link to={`products/${product.productID}`}>Show details</Link>}
+                    </Button>
+                    <Button
+
+                        variant="contained"
+                        color="secondary"
+                        fullWidth
+                        sx={{ marginTop: '16px' }}
+                        onClick={() => addToCart(product)}
+                    >
+                        Add To Cart
+                    </Button>
+                </CardContent>
+            </Card>
+        </Grid>
     );
 };
 
